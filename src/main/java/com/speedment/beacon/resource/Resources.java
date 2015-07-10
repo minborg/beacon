5d7808afb13eaabf9ab86e0a0ae9753b0342a92f
emilforslund <emil.forslund@pyknic.se>
2015-07-10 14:15:19 +0200

    Make Resources an enum

--- src/main/java/com/speedment/beacon/resource/Resources.java
+++ src/main/java/com/speedment/beacon/resource/Resources.java
@@ -5,15 +5,30 @@ import com.speedment.beacon.resource.content.MarioPng;
 import com.speedment.beacon.resource.content.NotFound_404;
 import com.speedment.beacon.resource.content.OneGif;
 import com.speedment.beacon.resource.content.OnePng;
+import com.speedment.beacon.resource.mime.MimeType;
+import java.io.InputStream;
 
-public class Resources {
+public enum Resources implements Resource {
+    
+    ONE_PNG       (new OnePng()),
+    ONE_GIF       (new OneGif()),
+    NOT_FOUND_404 (new NotFound_404()),
+    CATS_JPG      (new CatsJpg()),
+    MARIO_PNG     (new MarioPng());
+    
+    private final Resource resource;
 
-    private Resources() {}
+    private Resources(Resource res) {
+        resource = res;
+    }
 
-    public static final Resource ONE_PNG = new OnePng();
-    public static final Resource ONE_GIF = new OneGif();
-    public static final Resource NOT_FOUND_404 = new NotFound_404();
-    public static final Resource CATS_JPG = new CatsJpg();
-    public static final Resource MARIO_PNG = new MarioPng();
+    @Override
+    public MimeType getMimeType() {
+        return resource.getMimeType();
+    }
 
+    @Override
+    public InputStream newInputStream() {
+        return resource.newInputStream();
+    }
 }
